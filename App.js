b35e2f48f1951e1e5da0eda24097008d19e8f713
VictorbcSouza <victorsouza@MacBook-Pro-de-Fabio-2.local>
2020-12-26 20:09:14 -0300

    Praticando css inLine

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image} from 'react-native';
 class App extends Component{
   render(){
+
+    const img ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9eavvm6_TKHJOQv4N5tH9oHxzTOQzs5YOOQ&usqp=CAU';
     return(
       <View>
         <Text>Olá Mundo</Text>
-      </View>
+        <Text>Meu Primeiro App</Text>
+        <Text style={{ color: 'red', fontSize: 30}} >React-Native</Text>
+
+        <Image
+          source={{ uri: img}}
+          style={{width: 300, height: 250}}
+        />
+      </View>    
     );
   }
 }
