df2145143d81aa700ac9459af84393c03a1e600e
VictorBarboza <victorbcs22@gmail.com>
2021-01-06 20:02:35 -0300

    Entendendo as States

--- App.js
+++ App.js
@@ -3,20 +3,35 @@ import { View, Text, Image} from 'react-native';
 class App extends Component{
   render(){
 
-    const img ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9eavvm6_TKHJOQv4N5tH9oHxzTOQzs5YOOQ&usqp=CAU';
     return(
       <View>
         <Text>Olá Mundo</Text>
         <Text>Meu Primeiro App</Text>
         <Text style={{ color: 'purple', fontSize: 30}} >React-Native</Text>
+     
+      <Jobs largura ={500} altura ={200} nome="react-native" />
 
-        <Image
-          source={{ uri: img}}
-          style={{width: 300, height: 250}}
-        />
       </View>    
     );
   }
 }
 
+
+
 export default App;
+
+class Jobs extends Component{
+  render(){
+    const img ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9eavvm6_TKHJOQv4N5tH9oHxzTOQzs5YOOQ&usqp=CAU';
+
+    return(
+     <View> 
+        <Image
+        source={{ uri: img }}
+        style={{ width: this.props.largura, height: this.props.altura}}
+        />
+        <Text>React-Native</Text>
+      </View>
+    );  
+  }
+}
